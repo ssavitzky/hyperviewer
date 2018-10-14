714f4c793ff8879d2adbe9c64bbcd15cfd6287f6
Steve Savitzky <steve@savitzky.net>
2018-10-13 21:56:07 -0700

    Correct tests.
    I didn't know the correct matchers for true and false; they are
    .toBeTruthy() and .toBeFalsy().  The compiler doesn't complain about
    .toBeTrue, though, which complicated things.
    
    My hacky roughlyEqual test needed work, too.  There's toBeCloseTo, but
    the tests don't read as well if we use it.

--- src/polytopes.test.js
+++ src/polytopes.test.js
@@ -1,24 +1,46 @@
 import { cube, orthoplex, simplex, polytopeFactory } from './polytopes';
+import {abs} from 'math';
 
 it('has typed arrays', () => {
-    expect((typeof Float64Array).toBeDefined);
+    expect(typeof Float64Array).toBeDefined();
 });
 
 // verify that polytopes are initialized correctly
 
-// floating point equality is approximate
+// Note that we can use toBe(true) and toBe(false) with this,
+// because they return true and false explicitly.  Otherwise
+// we could use toBeTruthy() and toBeFalsy().
+
+// floating point equality is approximate.  We could 
 function roughlyEqual(v1, v2) {
-    return (v1 - v2)**2 < 1e-10;
+    return abs(v1 - v2) < 1e-10? true : false;
 }
 
-// test whether a vertex is on the unit sphere
+// test whether a vertex is on the unit sphere.  We could use
+// toBeCloseTo 
 function onUnitSphere(vertex) {
     let norm = 0.0;
-    for (let j = 0; j < vertex.size; j++) {
+    for (let j = 0; j < vertex.dimensions; ++j) {
 	norm += vertex.get(j) ** 2;
     }
     return roughlyEqual(norm, 1);
-} 
+}
+
+it('tests for roughlyEqual roughly correctly', () => {
+    expect(roughlyEqual(1, 0)).toBe(false);
+    expect(roughlyEqual(1, -1)).toBe(false);
+    expect(roughlyEqual(0, 0)).toBe(true);
+    expect(roughlyEqual(1, 1)).toBe(true);
+    expect(roughlyEqual(-1, -1)).toBe(true);
+    expect(roughlyEqual(0, 0)).toBe(true);
+    expect(roughlyEqual(1.99e-11, 0)).toBe(true);
+    expect(roughlyEqual(1, 0)).toBe(false);
+    expect(roughlyEqual(1, 0)).toBe(false);
+    expect(roughlyEqual(1, 1.1)).toBe(false);
+    expect(roughlyEqual(1.1, 1)).toBe(false);
+    expect(roughlyEqual(1e-4, 0)).toBe(false);
+    expect(roughlyEqual(0, 1e-4)).toBe(false);
+});
 
 it('makes cubes with the right number of vertices and edges', () => {
     for (let dim = 2; dim < 6; ++dim) {
@@ -58,18 +80,22 @@ it('puts vertices on the unit sphere', () => {
 	let theOrthoplex = new orthoplex(dim);
 	let theSimplex = new simplex(dim);
 	for (let v = 0; v < theCube.nVertices; ++v) {
-	    expect(onUnitSphere(theCube.vertices[v])).toBeTrue;
+	    expect(theCube.vertices[v].dimensions).toBe(dim);
+	    expect(onUnitSphere(theCube.vertices[v])).toBe(true);
 	}
 	for (let v = 0; v < theOrthoplex.nVertices; ++v) {
-	    expect(onUnitSphere(theOrthoplex.vertices[v])).toBeTrue;
+	    expect(theOrthoplex.vertices[v].dimensions).toBe(dim);
+	    expect(onUnitSphere(theOrthoplex.vertices[v])).toBe(true);
 	}
 	for (let v = 0; v < theSimplex.nVertices; ++v) {
-	    expect(onUnitSphere(theSimplex.vertices[v])).toBeTrue;
+	    expect(theSimplex.vertices[v].dimensions).toBe(dim);
+	    expect(theSimplex.nVertices).toBe(1 + dim);
+	    expect(onUnitSphere(theSimplex.vertices[v])).toBe(true);
 	}
     }
 });
 
-it('makes figures correctly', () => {
+it('makes figures correctly in a factory', () => {
     for (let d = 2; d < 4; d++) {
 	let factory = new polytopeFactory(d);
 	for (let f = 0; f < 3; f++) {
